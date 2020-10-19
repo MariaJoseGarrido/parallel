5142264dd5e67fd9a7bccf2dc9bbe30c6285d520
MariaJoseGarrido <mjgarrido.h@gmail.com>
2020-10-19 17:10:01 -0300

    links en json

--- src/Components/portafolio.jsx
+++ src/Components/portafolio.jsx
@@ -1,32 +1,32 @@
 import React from "react";
-import { CardImg, CardBody, CardSubtitle, CardText } from 'reactstrap'
-import {Proyects} from "../json/proyects.json"
+import { Link } from "react-router-dom";
+import { CardImg, CardBody, CardSubtitle, CardText } from "reactstrap";
+import { Proyects } from "../json/proyects.json";
 
 const Portafolio = () => {
   return (
-    
     <div className="container">
-  <div className="row">
-    {Proyects.map((element) => {
-      return (
-        <div className="col-md-6 col-lg-4">
-          <div className="card mx-0 my-3">
-            <CardImg src={element.img} alt="" />
-            <CardBody>
-              <CardSubtitle className="title">{element.title}</CardSubtitle>
-              <CardText> {element.description}</CardText>
-              <CardSubtitle className="title">{element.technology}</CardSubtitle>
-              <button type="button" class="btn btn-light">Demo</button>
-              <button type="button" class="btn btn-light">Repositorio</button>
-            </CardBody>
-          </div>
-        </div>
-      );
-    })}
-  </div>
-</div>
+      <div className="row">
+        {Proyects.map((element) => {
+          return (
+            <div className="col-md-6 col-lg-4">
+              <div className="card mx-0 my-3">
+                <CardImg src={element.img} alt="" />
+                <CardBody>
+                  <CardSubtitle className="title">{element.title}</CardSubtitle>
+                  <CardText> {element.description}</CardText>
+                  <CardSubtitle className="title">
+                    {element.technology}
+                  </CardSubtitle>
+                  <a href={element.demo}> Demo</a>
+                  <a href={element.repo}> Repositorio</a>
+                </CardBody>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 export default Portafolio;
-
-
